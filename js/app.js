3145d47f0ad5b04a8bea53f827c72e73d73fb48e
Ismail Uras <ismailuras09@gmail.com>
2023-08-02 21:31:41 +0300

    android smart banner display bug fix

--- js/app.js
+++ js/app.js
@@ -2,29 +2,36 @@ const openOnAppBtn = document.querySelector(".open_app button");
 openOnAppBtn.style.display = "none";
 
 let opSystem = null;
-const userAgent = window.navigator.userAgent;
+const userAgent = navigator.userAgent || window.opera;
 let screen;
 
 const getCurrentOpSystem = () => {
-  if (/android/i.test(userAgent)) {
-    opSystem = "Android";
-  } else if (/iPad|iPhone|iPod/i.test(userAgent)) {
-    opSystem = "iOS";
-  } else if (/windows phone/i.test(userAgent)) {
-    opSystem = "Windows Phone";
+  if (/windows phone/i.test(userAgent)) {
+    return (opSystem = "Windows Phone");
   }
+
+  if (/android/i.test(userAgent)) {
+    return (opSystem = "Android");
+  }
+
+  if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+    return (opSystem = "iOS");
+  }
+
+  return "unknown";
 };
 
 const handleDisplayOpenAppBtn = () => {
   const isAndroid = opSystem === "Android";
-  const isSmallScreen = window.matchMedia("screen and (max-width: 991.98px)").matches();
+  const isSmallScreen = window.matchMedia("screen and (max-width: 991.98px)").matches;
 
   if (isAndroid && isSmallScreen) {
     openOnAppBtn.style.display = "block";
   } else {
     openOnAppBtn.style.display = "none";
   }
-};
 
+  console.log(isSmallScreen, opSystem);
+};
 getCurrentOpSystem();
 handleDisplayOpenAppBtn();
