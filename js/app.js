291ff9bb8bfb3f7b8e7bc1b992913bdd7465b5a1
Ismail Uras <ismailuras09@gmail.com>
2023-08-24 22:07:29 +0300

    fix:/ default page changed. fix:/ privacy and terms page separated with links

--- js/app.js
+++ js/app.js
@@ -37,49 +37,6 @@ getCurrentOpSystem();
 window.addEventListener("resize", handleDisplayOpenAppBtn);
 window.addEventListener("DOMContentLoaded", handleDisplayOpenAppBtn);
 
-// modal
-
-const privacyLink = document.getElementById("privacy-link");
-const termsLink = document.getElementById("terms-link");
-const privacyModal = document.getElementById("privacy-modal");
-const termsModal = document.getElementById("terms-modal");
-const closeModal = document.querySelectorAll(".close-modal");
-
-const linkBtns = [privacyLink, termsLink];
-const closeIcons = closeModal;
-
-linkBtns.forEach((btn) => {
-  btn.addEventListener("click", (e) => {
-    const { value } = e.target;
-
-    if (value === "privacy") {
-      privacyModal.style.display = "flex";
-    } else {
-      termsModal.style.display = "flex";
-    }
-  });
-});
-
-closeIcons.forEach((icon) => {
-  icon.addEventListener("click", (e) => {
-    const { value } = e.target;
-
-    if (value === "privacy-close") {
-      privacyModal.style.display = "none";
-    } else {
-      termsModal.style.display = "none";
-    }
-  });
-});
-
-window.addEventListener("click", (event) => {
-  if (event.target === privacyModal) {
-    privacyModal.style.display = "none";
-  } else if (event.target === termsModal) {
-    termsModal.style.display = "none";
-  }
-});
-
 // language
 
 const langButtons = document.querySelectorAll(".lang_wrapper button");
